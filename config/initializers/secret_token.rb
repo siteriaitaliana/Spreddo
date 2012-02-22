9e6f2e0ab4275f1559a585520382e8dbb3582440
Lorenzo Urbini <superluglor@gmail.com>
2012-02-22 20:03:42 +0000

    First app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreddo::Application.config.secret_token = '813b6ee3db7a1623a627c73fd7b28db9303d2d61d660383413f82dee63a7565d935ffec5ea76e83107c808c066894433e92febe64d489c145d696c2ad34408ed'
